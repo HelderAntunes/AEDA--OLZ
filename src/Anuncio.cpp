516fb48dc8e601e3ada691778d56aa2536efe596
inesfproenca <inesfproenca@gmail.com>
2015-10-23 14:28:25 +0100

    Upgraded Anuncio

--- src/Anuncio.cpp
+++ src/Anuncio.cpp
@@ -6,21 +6,33 @@
  */
 
 #include"Anuncio.h"
+#include"Data.h"
 
 int Anuncio::identificador(0);
 
-Anuncio::Anuncio(string titulo,	string categoria,string descricao,/*imagens,*/int dia, int mes, int ano) : id(identificador ++){
+Anuncio::Anuncio(string titulo,	string categoria,string descricao,/*imagens,*/int dia, int mes, int ano) : id(identificador ++), data(Data(dia,mes,ano)), cat (Categoria(categoria)){
 	this->titulo = titulo;
-	this->categoria = categoria;
 	this->descricao = descricao;
-	this->data = Data(dia,mes,ano);
 	this->visualizacoes = 0;
 
 }
 
+Anuncio::Anuncio(string titulo, string categoria, string descricao,/*imagens*/ Data data): id(identificador ++), data(data), cat (Categoria(categoria)){
+	this->titulo = titulo;
+	this->descricao = descricao;
+	this->visualizacoes = 0;
+}
+
 DeVenda::DeVenda(string titulo,	string categoria,string descricao,/*imagens*/int dia,int mes,int ano,string estado,float preco, bool negociacao) :
 				Anuncio(titulo,categoria,descricao,/*imagens*/dia,mes,ano){
-	this->estado = estado;
+	if(estado == "novo")
+		this->estado = NOVO;
+	else if (estado == "usado")
+		this->estado = USADO;
+	else if (estado == "funcional")
+		this->estado = FUNCIONAL;
+	else if (estado == "pecas")
+		this->estado = PECAS;
 	this->preco = preco;
 	this->negociacao = negociacao;
 }
--- src/Anuncio.h
+++ src/Anuncio.h
@@ -13,11 +13,25 @@
 
 using namespace std;
 
+class Categoria{
+	string cat;
+	Categoria* subCategoria;
+public:
+	Categoria(string nome){
+		cat = nome;
+		subCategoria = NULL;
+	}
+	Categoria(string nome, Categoria* catPtr){
+		this->cat = nome;
+		this->subCategoria = catPtr;
+	}
+};
+
 class Anuncio{
 	static int identificador;
 protected:
 	string titulo;
-	string categoria;
+	Categoria cat;
 	string descricao;
 	//imagens
 	int id;
@@ -25,14 +39,17 @@ protected:
 	int visualizacoes;
 public:
 	Anuncio(string titulo, string categoria, string descricao,/*imagens*/int dia,int mes,int ano);
+	Anuncio(string titulo, string categoria, string descricao,/*imagens*/ Data data);
 };
 
 class DeVenda: public Anuncio{
-	string estado;
+public:
+	enum Estado {NOVO,USADO, FUNCIONAL, PECAS};
+	DeVenda(string titulo, string categoria, string descricao,/*imagens*/int dia,int mes,int ano,string estado,	float preco,bool negociacao);
+private:
+	Estado estado;
 	float preco;
 	bool negociacao;
-public:
-	DeVenda(string titulo, string categoria, string descricao,/*imagens*/int dia,int mes,int ano,string estado,	float preco,bool negociacao);
 };
 
 class DeCompra: public Anuncio{
