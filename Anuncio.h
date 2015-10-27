a603a42d73acf225b7f5128be73a3da02d94cba6
inesfproenca <inesfproenca@gmail.com>
2015-10-27 16:55:11 +0000

    Documented class Anuncio
    a classe Anuncio já está devidamente documentada.

--- Anuncio.h
+++ Anuncio.h
@@ -1,9 +1,8 @@
-/*
- * Anuncio.h
+/**
+ * @file Anuncio.h
  *
- *  Created on: 17/10/2015
- *      Author: Inês
- */
+ * @brief Header file for class Anuncio*/
+
 
 #ifndef SRC_ANUNCIO_H_
 #define SRC_ANUNCIO_H_
@@ -15,43 +14,116 @@
 
 using namespace std;
 
+/**@class Categoria
+ * @brief category class
+ * This class helps make the categories finite and uniform
+ */
 class Categoria{
-	string cat;
-	Categoria* subCategoria;
+	string cat;					/**< category name */
+	Categoria* subCategoria;	/**< pointer to subcategory */
 public:
+	/**
+	 * @brief class Categoria constructor.
+	 * Creates new category without subcategories
+	 *
+	 * @param nome category's name
+	 */
 	Categoria(string nome){
 		cat = nome;
 		subCategoria = NULL;
 	}
+	/**
+	 * @brief class Categoria constructor.
+	 * Creates new category with subcategories
+	 *
+	 * @param nome category's name
+	 * @param catPtr pointer to subcategory
+	 */
 	Categoria(string nome, Categoria* catPtr){
 		this->cat = nome;
 		this->subCategoria = catPtr;
 	}
+	/**
+	 * @brief get category's name.
+	 *
+	 * @return category's name
+	 */
 	string getCat(){return cat;}
 };
 
 enum Estado {NOVO,USADO, FUNCIONAL, PECAS};
 
+/**@class Anuncio
+ * @brief Advertisement class
+ */
 class Anuncio{
 	static int identificador;
 protected:
-	string titulo;
-	Categoria cat;
-	string descricao;
+	string titulo;			/**< advertisement's title */
+	Categoria cat;			/**< advertisement's category */
+	string descricao;		/**< advertisement's description */
 	//imagens
-	int id;
-	Data data;
-	int visualizacoes;
-	Utilizador* anunciante;
+	int id;					/**< advertisement's id */
+	Data data;				/**< advertisement's date of creation */
+	int visualizacoes;		/**< advertisement's number of views */
+	Utilizador* anunciante;	/**< pointer to the advertiser */
 public:
+	/**
+	 * @brief class Anuncio constructor.
+	 * Creates new advertisement with information provided and data correspondent to current date.
+	 *
+	 * @param titulo advertisement's title
+	 * @param categoria advertisement's category
+	 * @param descricao advertisement's description
+	 * @param anunciante pointer to the advertiser
+	 */
 	Anuncio(string titulo, string categoria, string descricao,/*imagens*/Utilizador* anunciante);
+	/**
+	 * @brief class Anuncio destructor.
+	 * A virtual destructor
+	 */
 	virtual ~Anuncio();
+	/**
+	 * @brief get title.
+	 *
+	 * @return advertisement's title
+	 */
 	string getTitulo();
+	/**
+	 * @brief get category.
+	 *
+	 * @return advertisement's category
+	 */
 	string getCategoria();
+	/**
+	 * @brief get id.
+	 *
+	 * @return advertisement's id
+	 */
 	int getId();
+	/**
+	 * @brief get date.
+	 *
+	 * @return advertisement's creation date
+	 */
 	Data getData();
+	/**
+	 * @brief get views.
+	 *
+	 * @return number of views gotten by the advertisement
+	 */
 	int getVisualizacoes();
+	/**
+	 * @brief increment views
+	 *
+	 * This function increments view counter by one
+	 */
 	void incVisualizacoes();
+	/**
+	 * @brief display ad
+	 *
+	 * Funtion display advertisement on screen
+	 */
 	virtual void imprime() const = 0;
 };
 
