85c00b3241d57850552af02c002e030e29fdb975
inesfproenca <inesfproenca@gmail.com>
2015-10-30 15:31:31 +0000

    Errors corrected

--- Anuncio.h
+++ Anuncio.h
@@ -15,48 +15,6 @@
 
 using namespace std;
 
-/**@class Categoria
- * This class helps make the categories finite and uniform
- * @brief category class.
- */
-class Categoria{
-	string catName;					/**< category name */
-	Categoria* subCategoria;	/**< pointer to subcategory */
-public:
-	/**
-	 * Creates new category without subcategories
-	 * @brief class Categoria constructor.
-	 *
-	 * @param nome category's name
-	 */
-	Categoria(string nome){
-		catName = nome;
-		subCategoria = NULL;
-	}
-	/**
-	 * Creates new category with subcategories
-	 * @brief class Categoria constructor.
-	 *
-	 * @param nome category's name
-	 * @param catPtr pointer to subcategory
-	 */
-	Categoria(string nome, Categoria* catPtr){
-		this->catName = nome;
-		this->subCategoria = catPtr;
-	}
-	/**
-	 * @brief display category on screen.
-	 *
-	 * @return output stream passed as parameter
-	 */
-	friend std::ostream& operator<< (std::ostream& out,const Categoria& category);
-};
-
-std::ostream& operator<< (std::ostream& out,const Categoria& category){
-	out << category.catName;
-	return out;
-}
-
 enum Estado {NOVO,USADO, FUNCIONAL, PECAS};
 
 class Utilizador;
@@ -68,7 +26,7 @@ class Anuncio{
 	static int identificador;
 protected:
 	string titulo;			/**< advertisement's title */
-	Categoria cat;			/**< advertisement's category */
+	string cat;				/**< advertisement's category */
 	string descricao;		/**< advertisement's description */
 	vector<string> imagens;	/**< vector of name of image files used*/
 	int id;					/**< advertisement's id */
@@ -76,11 +34,10 @@ protected:
 	int visualizacoes;		/**< advertisement's number of views */
 	Utilizador *anunciante;	/**< pointer to the advertiser */
 public:
-	Anuncio(string titulo, string categoria, string descricao, Utilizador* anunciante);
-	Anuncio(string titulo, string categoria, string descricao,vector<string> imagens, Utilizador* anunciante);
+	Anuncio(string titulo, string categoria, string descricao,vector<string> imagens, Data data, Utilizador* anunciante);
 	virtual ~Anuncio();
 	string getTitulo();
-	Categoria getCategoria();
+	string getCategoria();
 	int getId();
 	Data getData();
 	int getVisualizacoes();
@@ -90,7 +47,7 @@ public:
 	 * Function displays advertisement on screen
 	 * @brief display ad.
 	 */
-	virtual void imprime() const = 0;
+	virtual void imprime() const {}
 };
 
 /**@class DeVenda
@@ -101,7 +58,7 @@ class DeVenda: public Anuncio{
 	float preco;				/**< advertised object's demanded price */
 	bool negociacao;			/**< boolean indicating whether advertiser is willing to negotiate */
 public:
-	DeVenda(string titulo, string categoria, string descricao,vector<string> imagens, Estado estado, float preco,bool negociacao,Utilizador* anunciante);
+	DeVenda(string titulo, string categoria, string descricao,vector<string> imagens, Estado estado, float preco,bool negociacao, Data data, Utilizador* anunciante);
 	Estado getEstado();
 	float getPreco();
 	void setPreco(float preco);
@@ -114,8 +71,8 @@ public:
 class DeCompra: public Anuncio{
 	DeVenda* troca;				/**< pointer to an sale advertisement alerting for the possibility of an exchange */
 public:
-	DeCompra(string titulo, string categoria, string descricao, vector<string> imagens, DeVenda* ptr,Utilizador* anunciante);
-	DeCompra(string titulo, string categoria, string descricao, vector<string> imagens, Utilizador* anunciante);
+	DeCompra(string titulo, string categoria, string descricao, vector<string> imagens, DeVenda* ptr,Data data, Utilizador* anunciante);
+	DeCompra(string titulo, string categoria, string descricao, vector<string> imagens,Data data, Utilizador* anunciante);
 	void imprime() const ;
 };
 
